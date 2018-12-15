1f21d5385909e5026d024aa7d4ff571dde86eefe
patricio <patricio@idd.cl>
2018-12-14 22:36:21 -0300

    qwerty

---
+++ popa-4.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+
+# popa
+
+# Lorem ipsum es el texto que se usa habitualmente en diseño
+# gráfico en demostraciones de tipografías o de borradores
--- proa.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python
-
-# proa
-
-# de diseño para probar el diseño visual
-# antes de insertar el texto final. Wikipedia
